fadcd1bae1eb075014b41d3f2ae6fd9008987f3c
MarcM <marina.ved80gmail.com>
2024-08-19 19:26:04 -0600

    routes setup

--- server.js
+++ server.js
@@ -2,8 +2,9 @@
 const express = require('express');
 const path = require('path');
 
-const notesData = require('./db.json');
-const api = require('./routes/index.js');
+// const notesData = require('./db.json');
+const api = require('./routes/index');
+const apiNotes = require('./routes/notes');
 
 const app = express();
 const PORT = 3001;
@@ -16,17 +17,22 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // Send all 'localhost:3001/api' routes to the routes folder to use the index.js
 app.use('/api', api);
-
+app.use('/api/notes', apiNotes);
 
 //3.routes
 // sourse: UofU bootcamp, module 11, activity 05
 // a get URL 'localhost:3001/notes' request from user will bring/display to user the notes.html file 
 app.get('/notes', (req, res) =>
     res.sendFile(path.join(__dirname, 'public/notes.html'))
+
+    // display all existing notes in the left-hand column
+
+    // all nav-btn are not visible
+
   );
-  // a get URL 'localhost:3001/*' request from user will bring/display to user the index.html file. '*' - stands for any user input
+  // a get URL 'localhost:3001/*' request from user will bring/display to user the landing page (home-page) , which is index.html file. '*' - stands for any user input
   app.get('*', (req, res) =>
-    res.sendFile(path.join(__dirname, 'public/index.html'))
+    res.sendFile(path.join(__dirname, './public/index.html'))
   );
 
 //4.listen to a port 
